54a908da1a37303ebbf65fdf98a07321057921f1
Angel Martinez <amartinez@Angels-MacBook-Pro.local>
2017-10-06 10:53:58 -0400

    Fixed some typos

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Starting up EloStat Development Environment\n"
+echo "Starting up EloStat Development Environment"
 sudo apt update && sudo apt upgrade -y
 
 if command -v python3 > /dev/null 2>&1; then
-	echo "Python3 already installed.\n"
+	echo "Python3 already installed."
 else
 	sudo apt install python3 -y
 fi
 
 if command -v pip3 > /dev/null 2>&1; then
-	echo "Pip3 already installed.\n"
+	echo "Pip3 already installed."
 else
 	sudo apt install python3-pip -y
-	pip install virtualenv
+	pip3 install virtualenv
 fi
