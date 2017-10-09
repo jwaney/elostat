a7090d04abac976df6b58901c70ac63383dcbe7b
elo <angel@krodik.com>
2017-10-09 07:18:26 -0400

    removed print statements

--- elostat/outcomes/views.py
+++ elostat/outcomes/views.py
@@ -11,13 +11,10 @@ def redirection(request):
 
 def homepage(request, date_game):
 	all_games = OutCome.objects.all()
-	print(date_game)
 
 	d = datetime.date( int(date_game[:-4]) , int(date_game[4:-2]), int(date_game[-2:]))
 	filtered = []
 	for i in range(0, len(all_games)):
-		print('je')
-		print(all_games[i].date == d)
 		if(all_games[i].date == d):
 			filtered.append(all_games[i])
 			template = loader.get_template('outcomes/index.html')
